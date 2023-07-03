05af4e2122db1d9d5e9800774ed5ad68b78e1b11
Kagura0303 <133985670+Kagura0303@users.noreply.github.com>
2023-07-03 14:54:01 +0800

    3333

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+var imageUrls = [
+    "./1.png",
+    "./2.png",
+    "./3.png"
+  ];
+  
+  var currentImageIndex = 1;
+
+
 $(document).ready(function(){
     $("#btn1").click(function()
     {
-        $("#pic").fadeToggle();
+        $("#pic").fadeOut(500, function() {
+           
+            $(this).attr("src", imageUrls[currentImageIndex]);
+
+            $(this).fadeIn(500);
+
+            currentImageIndex = (currentImageIndex + 1) % imageUrls.length;
+
+          });
+          
     });
 });
