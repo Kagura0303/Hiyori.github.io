59742ca9ee7f8f591b9424c441ac71a8aec95d05
Kagura0303 <133985670+Kagura0303@users.noreply.github.com>
2023-07-03 17:18:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,20 @@ var imageUrls = [
     "./1.png",
     "./2.jpg",
     "./3.jpg",
-    "./4.jpg"
+    "./4.jpg",
+    "./5.jpg",
+    "./6.jpg",
+    "./7.jpg",
+    "./8.jpg",
+    "./9.jpg",
+    "./10.jpg",
+    "./11.jpg",
+    "./12.jpg",
+    "./13.jpg",
+    "./14.jpg",
+    "./15.jpg",
+    "./16.jpg",
+    "./17.jpg"
 ];
 
 var currentImageIndex = 0;
