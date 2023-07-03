4a49e63a5cc5ab88d00c39d7d58e0138062d2e04
Kagura0303 <133985670+Kagura0303@users.noreply.github.com>
2023-07-03 15:26:42 +0800

    2

--- index.js
+++ index.js
@@ -2,23 +2,22 @@ var imageUrls = [
     "./1.png",
     "./2.png",
     "./3.png"
-  ];
-  
-  var currentImageIndex = 1;
+];
 
+var currentImageIndex = 1;
 
 $(document).ready(function(){
-    $("#btn1").click(function()
-    {
+    $("#btn1").click(function() {
         $("#pic").fadeOut(500, function() {
-           
-            $(this).attr("src", imageUrls[currentImageIndex]);
+            currentImageIndex = (currentImageIndex - 1 + imageUrls.length) % imageUrls.length;
+            $("#pic").attr("src", imageUrls[currentImageIndex]).fadeIn(500);
+        });
+    });
 
-            $(this).fadeIn(500);
-
+    $("#btn2").click(function() {
+        $("#pic").fadeOut(500, function() {
             currentImageIndex = (currentImageIndex + 1) % imageUrls.length;
-
-          });
-          
+            $("#pic").attr("src", imageUrls[currentImageIndex]).fadeIn(500);
+        });
     });
 });
