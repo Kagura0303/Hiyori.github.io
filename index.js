e77dc74e37cf415d402671fccd68fbdcc91de31b
Kagura0303 <133985670+Kagura0303@users.noreply.github.com>
2023-07-03 16:40:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,42 +6,56 @@ var imageUrls = [
 ];
 
 var currentImageIndex = 0;
+var preloadedImages = [];
+
+// Preload images
+function preloadImages() {
+    for (var i = 0; i < imageUrls.length; i++) {
+        var image = new Image();
+        image.src = imageUrls[i];
+        preloadedImages.push(image);
+    }
+}
 
 $(document).ready(function(){
+    // Preload images on document ready
+    preloadImages();
+
     $("#btn1").click(function() {
         $("#pic").fadeOut(500, function() {
             currentImageIndex = (currentImageIndex - 1 + imageUrls.length) % imageUrls.length;
-            if (currentImageIndex === 0) { // Updated condition
-                $("#pic").css("width", ""); 
-                $("#pic").css("height", ""); 
-                $("#pic").css("left", "");
-                $("#pic").css("top", "");
+            if (currentImageIndex === 0) {
+                // Reset image styles if it's the first image
+                $("#pic").removeAttr("style");
             } else {
-                $("#pic").css("width", "235px"); 
-                $("#pic").css("height", "300px"); 
-                $("#pic").css("left", "42.8%");
-                $("#pic").css("top", "28%");
+                // Apply image styles if it's not the first image
+                $("#pic").css({
+                    "width": "235px",
+                    "height": "300px",
+                    "left": "42.8%",
+                    "top": "28%"
+                });
             }
-            $("#pic").attr("src", imageUrls[currentImageIndex]).fadeIn(500);
+            $("#pic").attr("src", preloadedImages[currentImageIndex].src).fadeIn(500);
         });
     });
 
     $("#btn2").click(function() {
         $("#pic").fadeOut(500, function() {
             currentImageIndex = (currentImageIndex + 1) % imageUrls.length;
-            if (currentImageIndex === 0) { // Updated condition
-                $("#pic").css("width", ""); 
-                $("#pic").css("height", ""); 
-                $("#pic").css("left", "");
-                $("#pic").css("top", "");
+            if (currentImageIndex === 0) {
+                // Reset image styles if it's the first image
+                $("#pic").removeAttr("style");
             } else {
-                $("#pic").css("width", "235px"); 
-                $("#pic").css("height", "300px"); 
-                $("#pic").css("left", "42.8%");
-                $("#pic").css("top", "28%");
+                // Apply image styles if it's not the first image
+                $("#pic").css({
+                    "width": "235px",
+                    "height": "300px",
+                    "left": "42.8%",
+                    "top": "28%"
+                });
             }
-            $("#pic").attr("src", imageUrls[currentImageIndex]).fadeIn(500);
+            $("#pic").attr("src", preloadedImages[currentImageIndex].src).fadeIn(500);
         });
     });
 });
-
