be9e7288ba4f593348cbcb2d5d0c45f05c192ebb
Kagura0303 <133985670+Kagura0303@users.noreply.github.com>
2023-07-03 16:08:19 +0800

    wwww

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ $(document).ready(function(){
     $("#btn1").click(function() {
         $("#pic").fadeOut(500, function() {
             currentImageIndex = (currentImageIndex - 1 + imageUrls.length) % imageUrls.length;
+            if (currentImageIndex > 0) {
+                
+                $("#pic").css("width", "235px"); 
+                $("#pic").css("height", "300px"); 
+                $("#pic").css("left", "42.8%");
+                $("#pic").css("top", "28%")
+            } else {
+                
+                $("#pic").css("width", ""); 
+                $("#pic").css("height", ""); 
+                $("#pic").css("left", "");
+                $("#pic").css("top", "")
+            }
             $("#pic").attr("src", imageUrls[currentImageIndex]).fadeIn(500);
         });
     });
@@ -18,6 +31,19 @@ $(document).ready(function(){
     $("#btn2").click(function() {
         $("#pic").fadeOut(500, function() {
             currentImageIndex = (currentImageIndex + 1) % imageUrls.length;
+            if (currentImageIndex > 0) {
+                
+                $("#pic").css("width", "235px"); 
+                $("#pic").css("height", "300px"); 
+                $("#pic").css("left", "42.8%");
+                $("#pic").css("top", "28%")
+            } else {
+                
+                $("#pic").css("width", ""); 
+                $("#pic").css("height", ""); 
+                $("#pic").css("left", "");
+                $("#pic").css("top", "")
+            }
             $("#pic").attr("src", imageUrls[currentImageIndex]).fadeIn(500);
         });
     });
